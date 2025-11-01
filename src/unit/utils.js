de052e18158eabdbf2d73e74a8a3eaabad9694fe
Tai Hong Huang <taihong.huang.ext@siemens.com>
2025-11-02 00:18:03 +0800

    天机函数

--- src/unit/utils.js
+++ src/unit/utils.js
@@ -59,3 +59,15 @@ export function getRandomAdByWeight(adArr) {
   }
   return adArr[adArr.length - 1]
 }
+
+/**
+ * 返回 [min, max] 区间的随机整数
+ * @param {number} min 最小值
+ * @param {number} max 最大值
+ * @returns {number}
+ */
+export function random(min, max) {
+  min = Math.ceil(min)
+  max = Math.floor(max)
+  return Math.floor(Math.random() * (max - min + 1)) + min
+}
